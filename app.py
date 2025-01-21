503118483fc2681537fd8a5e97c707b8891e9157
Umang <soyumang55@gmail.com>
2025-01-21 19:38:39 +0530

    retrieving html from url

--- app.py
+++ app.py
@@ -25,17 +25,17 @@ web_scraping_agent = Agent(
     model=GEMINI_MODEL,
     system_prompt=("""
         Your task is to extract relevant product information from the provided HTML content 
-    and return it as a list of dictionaries. Each dictionary should contain the following 
-    fields if they are present: 'brand_name', 'product_name', 'price', and 'rating_count'.
-    
-    Step 1: Use the `fetch_html_text(url)` function to fetch the HTML content from the provided URL. 
-    Step 2: Parse and extract the required fields from the HTML. 
-    Step 3: Clean the data by removing unnecessary HTML tags, scripts, and any irrelevant content.
-    Step 4: Return the extracted data as a list of dictionaries with the following format:
-      [
-        {"brand_name": "BrandA", "product_name": "Product 1", "price": "$100", "rating_count": 120},
-        ...
-      ]      
+        and return it as a list of dictionaries. Each dictionary should contain the following 
+        fields if they are present: 'brand_name', 'product_name', 'price', and 'rating_count'.
+
+        Step 1: Use the `fetch_html_text(url)` function to fetch the HTML content from the provided URL. 
+        Step 2: Parse and extract the required fields from the HTML. 
+        Step 3: Clean the data by removing unnecessary HTML tags, scripts, and any irrelevant content.
+        Step 4: Return the extracted data as a list of dictionaries with the following format:
+          [
+            {"brand_name": "BrandA", "product_name": "Product 1", "price": "$100", "rating_count": 120},
+            ...
+          ]      
     """),
     retries=3,
     result_type=Results,
@@ -44,3 +44,31 @@ web_scraping_agent = Agent(
         temperature=0.1
     )
 )
+
+# define custom function
+@web_scraping_agent.tool_plain(retries=1)
+def fetch_html_text(url: str) -> str:
+    """
+    Fetches the HTML text from a given URL
+
+    Args:
+        url: str - The page's URL to fetch the HTML text from
+
+    Returns:
+        str: The HTML text from the given URL
+    """
+    print('Calling URL: ',url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept-Language': 'en-US,en;q=0.5',
+    }
+    with Client(headers=headers) as client:
+        response = client.get(url, timeout=20)
+        if response.status_code != 200:
+            return f'Failed to fetch the HTML text from {url}. Status code: {response.status_code}'
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        with open('soup.txt', 'w', encoding='utf-8') as f:
+            f.write(str(soup))
+        print('Soup file saved')
+        return soup.get_text().replace('\n', '').replace('\r', '')
